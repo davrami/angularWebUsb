af94662b080ec39d5b3f9fc017bfaf6b0817c46f
aaronsalva27 <aaronsalva27@gmail.com>
2017-05-17 21:14:07 +0200

    led demo

--- src/components/demos/LED/LED.component.ts
+++ src/components/demos/LED/LED.component.ts
@@ -5,4 +5,126 @@ import { Component } from '@angular/core';
   styleUrls: ['./LED.style.css'],
   templateUrl: './LED.template.html'
 })
-export class LEDComponent { }
+export class LEDComponent {
+  sampleContent1 = `
+         <pre >
+            <code class="c highlight">
+  #include <WebUSB.h>
+
+  const WebUSBURL URLS[] = {
+    { 1, "webusb.github.io/arduino/demos/" },
+    { 0, "localhost:8000" },
+  };
+
+  const uint8_t ALLOWED_ORIGINS[] = { 1, 2 };
+
+  WebUSB WebUSBSerial(URLS, 2, 1, ALLOWED_ORIGINS, 2);
+
+  #define Serial WebUSBSerial
+
+  const int ledPin12 = 12;
+  const int ledPin11 = 11;
+  const int ledPin10 = 10;
+  const int ledPin9 = 9;
+  int ledPins[] = {
+    12, 11, 10, 9
+  };
+  int pinCount = 4;
+  int val=0;
+
+  void setup() {
+    while (!Serial) {
+      ;
+    }
+    Serial.begin(9600);
+    Serial.write("Sketch begins.\r\n> ");
+    Serial.flush();
+    pinMode(ledPin12, OUTPUT);
+    pinMode(ledPin11, OUTPUT);
+    pinMode(ledPin10, OUTPUT);
+    pinMode(ledPin9, OUTPUT);
+  }
+
+  void loop() {
+    if (Serial && Serial.available()) {
+    
+      int byte = Serial.read();
+
+      if (byte == '1') {
+        digitalWrite(ledPin12, HIGH);
+        val = digitalRead(ledPin12);
+        Serial.write("12");
+        Serial.write(val); 
+      } else if (byte == '2') {
+        digitalWrite(ledPin12, LOW);
+        val = digitalRead(ledPin12);
+        Serial.write("12");
+        Serial.write(val); 
+      }else if (byte == '3') {
+        digitalWrite(ledPin11, HIGH);
+        val = digitalRead(ledPin11);
+        Serial.write("11");
+        Serial.write(val); 
+      }else if (byte == '4') {
+        digitalWrite(ledPin11, LOW);
+        val = digitalRead(ledPin11);
+        Serial.write("11");
+        Serial.write(val);
+      }else if (byte == '5') {
+        digitalWrite(ledPin10, HIGH);
+        val = digitalRead(ledPin10);
+        Serial.write("10");
+        Serial.write(val);
+      }else if (byte == '6') {
+        digitalWrite(ledPin10, LOW);
+        val = digitalRead(ledPin10);
+        Serial.write("10");
+        Serial.write(val);
+      }else if (byte == '7') {
+        digitalWrite(ledPin9, HIGH);
+        val = digitalRead(ledPin9);
+        Serial.write("9");
+        Serial.write(val);
+      }else if(byte== '8'){
+        digitalWrite(ledPin9, LOW);
+        val = digitalRead(ledPin9);
+        Serial.write("9");
+        Serial.write(val);
+      }else {
+        for (int thisPin = 0; thisPin < pinCount; thisPin++) {
+          digitalWrite(ledPins[thisPin], HIGH);
+          val = digitalRead(ledPins[thisPin]);   // read the input pin
+          Serial.write(val);        
+          delay(500);
+          digitalWrite(ledPins[thisPin], LOW);
+        }
+
+      
+      }
+
+    Serial.flush();
+    }
+
+
+  }
+            </code>
+        </pre>
+        
+        `;
+
+  sampleContent2 = `
+         <pre>
+            <code class="typescript highlight">
+                class Greeter {
+                    constructor(public greeting: string) { }
+                    greet() {
+                        return "hello world";
+                    }
+                };
+            </code>
+        </pre>
+        
+        `;
+
+
+}
